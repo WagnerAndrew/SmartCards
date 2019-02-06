8012b29740a8dbcf2ea54c6b654212b61d8da944
WagnerAndy <wagner.andrew.james@gmail.com>
2019-02-06 12:38:39 -0700

    Additional bug fixes - need seeds

---
+++ scripts/cardSeeds.js
@@ -0,0 +1,65 @@
+
+const mongoose = require("mongoose");
+const db = require("../models");
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/codecards"
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log('MongoDB Seeded...'))
+​
+
+​
+let libSeed = {
+    library: "Seed Library",
+    cardsID: [ ]
+ };
+​
+const cardSeed = [
+    {
+      question: "What does each letter of the MERN acronym represent?",
+      answer: "MongoDB, Express, React, and Node"
+    },
+    {
+      question: "What is Node.js?",
+      answer: "Node.js is a JavaScript run-time environment that executes JavaScript code outside of a browser"
+    },
+    {
+      question: "ES6/ES2015 is the sixth major release of the ECMAScript language specification which is commonly known as ___?",
+      answer: "JavaScript"
+    },
+    {
+      question: "What is the primary purpose of a transpiling tool like Babel?",
+      answer: "To convert newer unsupported JS to JS the browser understands"
+    },
+  ];
+​
+db.Card
+ .deleteMany({})
+ .then(() => db.Card.collection.insertMany(cardSeed))
+ .then(data => {
+  console.log(data.result.n + " cards inserted!");
+  // console.log(data.insertedIds);
+  for (let key in data.insertedIds) {
+   libSeed.cardsID.push(data.insertedIds[key]);
+  }
+  db.Library
+   .deleteMany({})
+   .then(() => db.Library.collection.insertOne(libSeed))
+   .then(data => {
+    console.log(data.result.n + " library inserted!");
+    console.log("Library _ID: ", data.insertedId);
+    console.log("Library Data:", data.ops[0]);
+    process.exit(0);
+   })
+   .catch(err => {
+    console.error(err);
+    process.exit(1);
+   })
+  // process.exit(0);
+ })
+ .catch(err => {
+  console.error(err);
+  process.exit(1);
+ });
+
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,27 +10,31 @@ mongoose.connect(
 
 const cardSeed = [
   {
-    question: "What does 2+2 equal?",
-    answer: "4",
-    guess: false
+    question: "What does each letter of the MERN acronym represent?",
+    answer: "MongoDB, Express, React, and Node"
   },
   {
-    question: "Apple or orange today?",
-    answer: "Apple",
-    guess: false
+    question: "What is Node.js?",
+    answer: "Node.js is a JavaScript run-time environment that executes JavaScript code outside of a browser"
   },
   {
-    question: "Sleep or study?",
-    answer: "Sleep",
-    guess: false
+    question: "ES6/ES2015 is the sixth major release of the ECMAScript language specification which is commonly known as ___?",
+    answer: "JavaScript"
+  },
+  {
+    question: "What is the primary purpose of a transpiling tool like Babel?",
+    answer: "To convert newer unsupported JS to JS the browser understands"
   },
 ];
 
 db.Card
   .remove({})
   .then(() => db.Card.collection.insertMany(cardSeed))
+  .then ( (results) =>
+   {console.log ("Results is: ", results)
+    db.Library.findOneAndUpdate({library: "Web Development" }, { $push: { cardsID: results.insertedIds }}, { new: true })})
   .then(data => {
-    console.log(data.result.n + " records inserted!");
+    console.log(data.result + " records inserted!");
     process.exit(0);
   })
   .catch(err => {
