0a096530424c613d49086eabd5893f3c55eec182
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-02-22 15:53:04 -0700

    Original server

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 const express = require("express");
+
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const path =  require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3001
-;
+const PORT = process.env.PORT || 3001;
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/smartcards");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/', routes);
-
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
 }
+
+app.use(routes);
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/smartcards");
+
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
