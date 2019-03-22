48e0d54f5f552e148b7bd8f5670445fbc2267948
WagnerAndrew <wagner.andrew.james@gmail.com>
2019-03-22 13:19:11 -0600

    Fixed Next button

--- client/src/components/Card/CardButtons.js
+++ client/src/components/Card/CardButtons.js
@@ -1,62 +1,78 @@
-import React, { Component } from 'react'
-import { Button, Icon, Row, Col } from 'react-materialize'
-
+import React, { Component } from 'react';
+import { Button, Icon, Row, Col } from 'react-materialize';
 
 export class CardButtons extends Component {
+  incorrect = e => {
+    e.preventDefault();
+    this.props.incorrect();
+  };
 
+  nextCard = e => {
+    e.preventDefault();
+    this.props.nextCard();
+  };
 
-    incorrect = e => {
-        e.preventDefault();
-        this.props.incorrect();
-    }
+  correct = e => {
+    e.preventDefault();
+    this.props.correct();
+  };
 
-    nextCard = e => {
-        e.preventDefault();
-        this.props.nextCard();
-    }
+  results = e => {
+    e.preventDefault();
+    this.props.results();
+  };
 
-
-    correct = e => {
-        e.preventDefault();
-        this.props.correct();
-    }
-
-    results = e => {
-        e.preventDefault();
-        this.props.results();
-    }
-
-
-    render() {
-        return (
-            <>
-                <Row className= "center-align">
-                    <Col s={4} >
-                        <Button waves='light' className="red lighten" disabled={this.props.disabled} onClick={this.incorrect}>
-                            <Icon>thumb_down</Icon>
-                        </Button>
-                    </Col>
-                    <Col s={4}>
-                        <Button waves='light' className="grey lighten-3 black-text" onClick={this.nextCard}>
-                            Next Card
-                    </Button>
-                    </Col>
-                    <Col s={4} >
-                        <Button waves='light' className="green lighten" disabled={this.props.disabled} onClick={this.correct}>
-                            <Icon>thumb_up</Icon>
-                        </Button>
-                    </Col>
-                </Row>
-                <Row className= "center-align">
-                    <Col s={12} >
-                        <Button waves='light' flat={true} className="grey lighten-5 black-text" onClick={this.results} >
-                            Finish
-                        </Button>
-                    </Col>
-                </Row>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Row className='center-align'>
+          <Col s={4}>
+            <Button
+              waves='light'
+              className='red lighten'
+              disabled={this.props.disabled}
+              onClick={this.incorrect}
+            >
+              <Icon>thumb_down</Icon>
+            </Button>
+          </Col>
+          <Col s={4}>
+            <Button
+              waves='light'
+              className='grey lighten-3 black-text'
+              disabled={this.props.showNext}
+              onClick={this.nextCard}
+            >
+              Next Card
+            </Button>
+          </Col>
+          <Col s={4}>
+            <Button
+              waves='light'
+              className='green lighten'
+              disabled={this.props.disabled}
+              onClick={this.correct}
+            >
+              <Icon>thumb_up</Icon>
+            </Button>
+          </Col>
+        </Row>
+        <Row className='center-align'>
+          <Col s={12}>
+            <Button
+              waves='light'
+              flat={true}
+              className='grey lighten-5 black-text'
+              disabled={this.props.showFinish}
+              onClick={this.results}
+            >
+              Finish
+            </Button>
+          </Col>
+        </Row>
+      </>
+    );
+  }
 }
 
-export default CardButtons
+export default CardButtons;
--- client/src/pages/Libraries.js
+++ client/src/pages/Libraries.js
@@ -1,180 +1,216 @@
 import React, { Component } from 'react';
-import { Row } from 'react-materialize'
+import { Row } from 'react-materialize';
 import Card from '../components/Card/Card';
-import CardButtons from '../components/Card/CardButtons'
-import { CorrectAnswers, IncorrectAnswers } from '../components/Card/Results'
-import API from '../utils/API'
+import CardButtons from '../components/Card/CardButtons';
+import { CorrectAnswers, IncorrectAnswers } from '../components/Card/Results';
+import API from '../utils/API';
 
 export class Libraries extends Component {
+  state = {
+    libraries: [],
+    cards: [],
+    correct: [],
+    incorrect: [],
+    question: '',
+    answer: '',
+    index: 0,
+    libraryName: '',
+    resultText: '',
+    disabled: false,
+    showNext: false,
+    showFinish: false,
+    step: 1
+  };
 
-    state = {
-        libraries: [],
-        cards: [],
-        correct: [],
-        incorrect: [],
-        question: '',
-        answer: '',
-        index: 0,
-        libraryName: '',
-        resultText: '',
-        disabled: false,
-        step: 1
+  componentDidMount() {
+    this.loadLibraries();
+  }
 
-    }
+  loadLibraries = () => {
+    API.getLibraries()
+      .then(res =>
+        this.setState({
+          libraries: res.data,
+          index: 0,
+          resultText: '',
+          step: 1
+        })
+      )
+      .catch(err => console.log(err));
+  };
 
-    componentDidMount() {
-        this.loadLibraries();
-    }
+  loadCards = (id, name) => {
+    const { index, step } = this.state;
+    API.getLibrary(id)
+      .then(res =>
+        this.setState({
+          correct: [],
+          incorrect: [],
+          cards: res.data.cardsID,
+          libraryName: name,
+          question: res.data.cardsID[index].question,
+          answer: res.data.cardsID[index].answer,
+          showNext: true,
+          showFinish: true,
+          disabled: false,
+          step: step + 1
+        })
+      )
+      .catch(err => console.log(err));
+  };
 
-    loadLibraries = () => {
-        API.getLibraries()
-            .then(res =>
-                this.setState({
-                    libraries: res.data,
-                    index: 0,
-                    resultText: '',
-                    step: 1
+  nextCard = () => {
+    const { index, cards } = this.state;
+    console.log(index, cards.length);
 
-                })
-            )
-            .catch(err => console.log(err)
-            );
-    };
-
-    loadCards = (id, name) => {
-        const { index, step } = this.state;
-        API.getLibrary(id)
-            .then(res =>
-                this.setState({
-                    correct: [],
-                    incorrect: [],
-                    cards: res.data.cardsID,
-                    libraryName: name,
-                    question: res.data.cardsID[index].question,
-                    answer: res.data.cardsID[index].answer,
-                    disabled: false,
-                    step: step + 1
-                })
-            )
-            .catch(err => console.log(err)
-            );
-    };
-
-    nextCard = () => {
-        const { index, cards } = this.state;
-        console.log(index, cards.length);
-
-        if (index === cards.length - 1) {
-            
-            this.results();
-
-        } else {
-            this.setState({
-                index: index + 1,
-                resultText: ''
-            }, () => {
-                this.refreshCard()
-            })
+    if (index === cards.length - 1) {
+      this.results();
+    } else {
+      this.setState(
+        {
+          index: index + 1,
+          resultText: ''
+        },
+        () => {
+          this.refreshCard();
         }
-
-    };
-
-    refreshCard = () => {
-        const { index, cards } = this.state;
-        this.setState({
-            question: cards[index].question,
-            answer: cards[index].answer, 
-            disabled: false
-        })
-    };
-
-    correct = () => {
-
-        const { question, answer } = this.state;
-
-        this.setState({
-            disabled: true,
-            resultText: "Got it!"
-        }, () => {
-            this.state.correct.push({ question: question, answer: answer })
-        })
-
-    };
-
-    incorrect = () => {
-        const { question, answer } = this.state;
-
-        this.setState({
-            disabled: true,
-            resultText: "Missed it!"
-        }, () => {
-            this.state.incorrect.push({ question: question, answer: answer })
-        })
-    };
-
-    results = () => {
-        const { step } = this.state;
-        this.setState({
-            step: step + 1
-        })
+      );
     }
+  };
 
-    render() {
-        const { step, question, answer, disabled } = this.state;
+  refreshCard = () => {
+    const { index, cards } = this.state;
+    this.setState({
+      question: cards[index].question,
+      answer: cards[index].answer,
+      showNext: true,
+      showFinish: false,
+      disabled: false
+    });
+  };
 
-        switch (step) {
-            case 1:
-                return (
-                    <Row>
-                        <span className="black-text center-align"><h4>Select A Library To Study</h4></span>
+  correct = () => {
+    const { question, answer } = this.state;
 
-                        <div className="center-align libraries">
-                            {this.state.libraries.map(button => (
-                                <ul key={button._id}>
-                                    <li>
-                                        <h5 onClick={() => this.loadCards(button._id, button.library)} className="libraryList grey-text text-darken-2">{button.library}</h5>
-                                    </li>
-                                </ul>
-                            ))}
-                        </div>
-                    </Row>
-                )
-            case 2:
-                return (
-                    <>
-                        <span className="black-text center-align"><h4>{this.state.libraryName}</h4></span>
-                        <Card question={question} answer={answer} />
-                        <CardButtons nextCard={this.nextCard} disabled={disabled} correct={this.correct} incorrect={this.incorrect} results={this.results} />
-                        <span className="black-text center-align"><h5>{this.state.resultText}</h5></span>
-                    </>
-                )
-            case 3:
-                return (
-                    <>
-                        <span className="black-text center-align"><h4>{this.state.libraryName}</h4></span>
-                        <span className="black-text center-align"><h5>Results</h5></span>
-                        <CorrectAnswers>
+    this.setState(
+      {
+        disabled: true,
+        showNext: false,
+        resultText: 'Got it!'
+      },
+      () => {
+        this.state.correct.push({ question: question, answer: answer });
+      }
+    );
+  };
 
-                            {this.state.correct.map(table => (
-                                <tr>
-                                    <td>{table.question}</td>
-                                    <td>{table.answer}</td>
-                                </tr>
-                            ))}
-                        </CorrectAnswers>
-                        <IncorrectAnswers>
-                            {this.state.incorrect.map(table => (
-                                <tr>
-                                    <td>{table.question}</td>
-                                    <td>{table.answer}</td>
-                                </tr>
-                            ))}
-                        </IncorrectAnswers>
-                    </>
-                )
-            default: return null;
-        }
+  incorrect = () => {
+    const { question, answer } = this.state;
+
+    this.setState(
+      {
+        disabled: true,
+        showNext: false,
+        resultText: 'Missed it!'
+      },
+      () => {
+        this.state.incorrect.push({ question: question, answer: answer });
+      }
+    );
+  };
+
+  results = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  render() {
+    const {
+      step,
+      question,
+      answer,
+      showNext,
+      showFinish,
+      disabled
+    } = this.state;
+
+    switch (step) {
+      case 1:
+        return (
+          <Row>
+            <span className='black-text center-align'>
+              <h4>Select A Library To Study</h4>
+            </span>
+
+            <div className='center-align libraries'>
+              {this.state.libraries.map(button => (
+                <ul key={button._id}>
+                  <li>
+                    <h5
+                      onClick={() => this.loadCards(button._id, button.library)}
+                      className='libraryList grey-text text-darken-2'
+                    >
+                      {button.library}
+                    </h5>
+                  </li>
+                </ul>
+              ))}
+            </div>
+          </Row>
+        );
+      case 2:
+        return (
+          <>
+            <span className='black-text center-align'>
+              <h4>{this.state.libraryName}</h4>
+            </span>
+            <Card question={question} answer={answer} />
+            <CardButtons
+              nextCard={this.nextCard}
+              showNext={showNext}
+              showFinish={showFinish}
+              disabled={disabled}
+              correct={this.correct}
+              incorrect={this.incorrect}
+              results={this.results}
+            />
+            <span className='black-text center-align'>
+              <h5>{this.state.resultText}</h5>
+            </span>
+          </>
+        );
+      case 3:
+        return (
+          <>
+            <span className='black-text center-align'>
+              <h4>{this.state.libraryName}</h4>
+            </span>
+            <span className='black-text center-align'>
+              <h5>Results</h5>
+            </span>
+            <CorrectAnswers>
+              {this.state.correct.map(table => (
+                <tr>
+                  <td>{table.question}</td>
+                  <td>{table.answer}</td>
+                </tr>
+              ))}
+            </CorrectAnswers>
+            <IncorrectAnswers>
+              {this.state.incorrect.map(table => (
+                <tr>
+                  <td>{table.question}</td>
+                  <td>{table.answer}</td>
+                </tr>
+              ))}
+            </IncorrectAnswers>
+          </>
+        );
+      default:
+        return null;
     }
+  }
 }
-export default Libraries
-
+export default Libraries;
